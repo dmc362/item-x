ff8f7e0ba096c2e3501692a1ebe06b335e1323ae
username <1377715736@qq.com>
2019-03-23 07:42:56 +0800

    2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    margin: 10,
+    nav: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    lazyLoad: true
+  });
+});
